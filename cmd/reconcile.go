c67de4680df9cfe4bff86e75846040de7ed9569f
Aaron Hulse <aaron.hulse@questanalytics.com>
2025-04-10 13:25:28 -0500

    Convert to structured logging

--- cmd/reconcile.go
+++ cmd/reconcile.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	log "github.com/sirupsen/logrus"
 	"math"
 	"strconv"
 	"time"
@@ -18,71 +19,87 @@ func (a *PVCAutoscaler) reconcile(ctx context.Context) error {
 	if err != nil {
 		return fmt.Errorf("could not get PersistentVolumeClaims: %w", err)
 	}
-	a.logger.Debugf("fetched %d annotated pvcs", pvcl.Size())
+	a.logger.WithField("count", pvcl.Size()).Debug("fetched annotated pvcs")
 
 	pvcsMetrics, err := a.metricsClient.FetchPVCsMetrics(ctx, time.Now())
 	if err != nil {
-		a.logger.Errorf("could not fetch the PersistentVolumeClaims metrics: %v", err)
+		a.logger.WithField("error", err).Error("could not fetch the PersistentVolumeClaims metrics")
 		return nil
 	}
 	a.logger.Debug("fetched pvc metrics")
 
 	for _, pvc := range pvcl.Items {
 		pvcId := fmt.Sprintf("%s/%s", pvc.Namespace, pvc.Name)
-		a.logger.Debugf("processing pvc %s", pvcId)
+		a.logger.WithField("id", pvcId).Debug("processing pvc")
 
 		// Determine if the StorageClass allows volume expansion
 		storageClassName := *pvc.Spec.StorageClassName
 		isExpandable, err := isStorageClassExpandable(ctx, a.kubeClient, storageClassName)
 		if err != nil {
-			a.logger.Errorf("could not get StorageClass %s for %s: %v", storageClassName, pvcId, err)
+			a.logger.WithFields(log.Fields{
+				"storageClassName": storageClassName,
+				"pvcId":            pvcId,
+				"error":            err,
+			}).Error("failed getting StorageClass")
 			continue
 		}
 		if !isExpandable {
-			a.logger.Errorf("the StorageClass %s of %s does not allow volume expansion", storageClassName, pvcId)
+			a.logger.WithFields(log.Fields{
+				"storageClassName": storageClassName,
+				"pvcId":            pvcId,
+			}).Error("the StorageClass does not allow volume expansion")
 			continue
 		}
-		a.logger.Debugf("storageclass for %s allows volume expansion", pvcId)
+		a.logger.WithField("id", pvcId).Debug("storageclass allows volume expansion")
 
 		// Determine if pvc the meets the condition for resize
 		err = isPVCResizable(&pvc)
 		if err != nil {
-			a.logger.Errorf("the PersistentVolumeClaim %s is not resizable: %v", pvcId, err)
+			a.logger.WithFields(log.Fields{
+				"pvcId": pvcId,
+				"error": err,
+			}).Error("the PersistentVolumeClaim %s is not resizable: %v")
 			continue
 		}
-		a.logger.Debugf("pvc %s meets the resizing conditions", pvcId)
+		a.logger.WithField("id", pvcId).Debug("pvc meets the resizing conditions")
 
 		namespacedName := types.NamespacedName{
 			Namespace: pvc.Namespace,
 			Name:      pvc.Name,
 		}
 		if _, ok := pvcsMetrics[namespacedName]; !ok {
-			a.logger.Errorf("could not fetch the metrics for %s", pvcId)
+			a.logger.WithField("id", pvcId).Error("could not fetch the metrics for pvc")
 			continue
 		}
-		a.logger.Debugf("metrics for %s received", pvcId)
+		a.logger.WithField("id", pvcId).Debug("metrics for pvc received")
 
 		pvcCurrentCapacityBytes := pvcsMetrics[namespacedName].VolumeCapacityBytes
 
 		threshold, err := convertPercentageToBytes(pvc.Annotations[PVCAutoscalerThresholdAnnotation], pvcCurrentCapacityBytes, DefaultThreshold)
 		if err != nil {
-			a.logger.Errorf("failed to convert threshold annotation for %s: %v", pvcId, err)
+			a.logger.WithFields(log.Fields{
+				"pvcId": pvcId,
+				"error": err,
+			}).Error("failed to convert threshold annotation")
 			continue
 		}
 
 		capacity, exists := pvc.Status.Capacity[corev1.ResourceStorage]
 		if !exists {
-			a.logger.Infof("skip %s because its capacity is not set yet", pvcId)
+			a.logger.WithField("id", pvcId).Info("skip pvc because its capacity is not set yet")
 			continue
 		}
 		if capacity.Value() == 0 {
-			a.logger.Infof("skip %s because its capacity is zero", pvcId)
+			a.logger.WithField("id", pvcId).Info("skip pvc because its capacity is zero")
 			continue
 		}
 
 		increase, err := convertPercentageToBytes(pvc.Annotations[PVCAutoscalerIncreaseAnnotation], capacity.Value(), DefaultIncrease)
 		if err != nil {
-			a.logger.Errorf("failed to convert increase annotation for %s: %v", pvcId, err)
+			a.logger.WithFields(log.Fields{
+				"pvcId": pvcId,
+				"error": err,
+			}).Error("failed to convert increase annotation")
 			continue
 		}
 
@@ -90,28 +107,34 @@ func (a *PVCAutoscaler) reconcile(ctx context.Context) error {
 		if exist {
 			parsedPreviousCapacity, err := strconv.ParseInt(previousCapacity, 10, 64)
 			if err != nil {
-				a.logger.Errorf("failed to parse 'previous_capacity' annotation: %v", err)
+				a.logger.WithField("error", err).Error("failed to parse 'previous_capacity' annotation")
 				continue
 			}
 			if parsedPreviousCapacity == pvcCurrentCapacityBytes {
-				a.logger.Infof("pvc %s is still waiting to accept the resize", pvcId)
+				a.logger.WithField("id", pvcId).Info("pvc is still waiting to accept the resize")
 				continue
 			}
 		}
 
 		ceiling, err := getPVCStorageCeiling(&pvc)
 		if err != nil {
-			a.logger.Errorf("failed to fetch storage ceiling for %s: %v", pvcId, err)
+			a.logger.WithFields(log.Fields{
+				"pvcId": pvcId,
+				"error": err,
+			}).Error("failed to fetch storage ceiling")
 			continue
 		}
 		if capacity.Cmp(ceiling) >= 0 {
-			a.logger.Infof("volume storage limit (%s) reached for %s", ceiling.String(), pvcId)
+			a.logger.WithFields(log.Fields{
+				"pvcId": pvcId,
+				"limit": ceiling.String(),
+			}).Info("volume storage limit reached")
 			continue
 		}
 
 		currentUsedBytes := pvcsMetrics[namespacedName].VolumeUsedBytes
 		if currentUsedBytes >= threshold {
-			a.logger.Infof("pvc %s usage bigger than threshold", pvcId)
+			a.logger.WithField("id", pvcId).Info("pvc usage bigger than threshold")
 
 			// 1<<30 is a bit shift operation that represents 2^30, i.e. 1Gi
 			newStorageBytes := int64(math.Ceil(float64(capacity.Value()+increase)/(1<<30))) << 30
@@ -122,10 +145,17 @@ func (a *PVCAutoscaler) reconcile(ctx context.Context) error {
 
 			err := a.updatePVCWithNewStorageSize(ctx, &pvc, pvcCurrentCapacityBytes, newStorage)
 			if err != nil {
-				a.logger.Errorf("failed to resize pvc %s: %v", pvcId, err)
+				a.logger.WithFields(log.Fields{
+					"pvcId": pvcId,
+					"error": err,
+				}).Error("failed to resize pvc")
 			}
 
-			a.logger.Infof("pvc %s resized from %d to %d ", pvcId, capacity.Value(), newStorage.Value())
+			a.logger.WithFields(log.Fields{
+				"pvcId": pvcId,
+				"old":   capacity.Value(),
+				"new":   newStorage.Value(),
+			}).Info("pvc resized")
 		}
 	}
 
@@ -138,13 +168,13 @@ func (a *PVCAutoscaler) updatePVCWithNewStorageSize(ctx context.Context, pvcToRe
 	pvcToResize.Spec.Resources.Requests[corev1.ResourceStorage] = *newStorageBytes
 
 	pvcToResize.Annotations[PVCAutoscalerPreviousCapacityAnnotation] = strconv.FormatInt(capacityBytes, 10)
-	a.logger.Debugf("PVCAutoscalerPreviousCapacityAnnotation annotation written for %s ok", pvcId)
+	a.logger.WithField("id", pvcId).Debug("PVCAutoscalerPreviousCapacityAnnotation annotation written")
 
 	_, err := a.kubeClient.CoreV1().PersistentVolumeClaims(pvcToResize.Namespace).Update(ctx, pvcToResize, metav1.UpdateOptions{})
 	if err != nil {
 		return fmt.Errorf("failed to update PVC %s: %w", pvcId, err)
 	}
-	a.logger.Debugf("update function called and returned no error for %s ok", pvcId)
+	a.logger.WithField("id", pvcId).Debug("update function called and returned no error")
 
 	return nil
 }
